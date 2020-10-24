31938aa69409ec668aff3ae90d1109db1312354e
fadi <fadeomar2015@gmail.com>
2020-10-24 05:26:24 +0300

    add the sign in component to the login page

---
+++ src/pages/Login/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SignIn from "../../components/SignIn";
+
+import "./style.scss";
+
+const Login = () => (
+  <div>
+    <SignIn />
+  </div>
+);
+
+export default Login;
