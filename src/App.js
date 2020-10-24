28b8565d73b2898090d9ecfc965ce8f95dbc2036
fadi <fadeomar2015@gmail.com>
2020-10-24 04:31:29 +0300

    using switch to render the routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage/HomePage.component";
+import Shop from "./pages/Shop/Shop.component";
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={Shop} />
+      </Switch>
     </div>
   );
 }
