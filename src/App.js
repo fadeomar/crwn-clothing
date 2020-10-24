2ac0b6dd1b4ad41d6f709bcd9817696b4f84947a
fadi <fadeomar2015@gmail.com>
2020-10-24 05:26:36 +0300

    import the login page to the app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import HomePage from "./pages/Homepage/HomePage.component";
 import Shop from "./pages/Shop/Shop.component";
 import Header from "./components/Header";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={Shop} />
+        <Route exact path="/login" component={Login} />
       </Switch>
     </div>
   );
