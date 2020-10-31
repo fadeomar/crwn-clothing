b2438e04c9464d57994e30106f4c02fe1d4fcaed
fadi <fadeomar2015@gmail.com>
2020-10-31 13:08:24 +0200

    create auth state in the app

--- src/App.js
+++ src/App.js
@@ -6,17 +6,37 @@ import Shop from "./pages/Shop/Shop.component";
 import Header from "./components/Header";
 import Login from "./pages/Login";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={Shop} />
-        <Route exact path="/login" component={Login} />
-      </Switch>
-    </div>
-  );
+import { auth } from "./utils/firebase";
+
+class App extends React.Component {
+  state = {
+    currentUser: null,
+  };
+
+  unSubscripeFromAuth = null;
+
+  componentDidMount() {
+    this.unSubscripeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unSubscripeFromAuth = null;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={Shop} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
