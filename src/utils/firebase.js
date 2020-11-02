b88f085f6b240280c7aa1193ae916e03061b1028
fadi <fadeomar2015@gmail.com>
2020-11-02 15:02:44 +0200

    create sign up with email and password method using firebase

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -16,9 +16,40 @@ const config = {
 };
 
 firebase.initializeApp(config);
-
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+
+export const createUserProfileDoc = async (userAuth, additonalData) => {
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additonalData,
+      });
+    } catch (e) {
+      console.log("error creating user", e.message);
+    }
+  }
+  return userRef;
+};
+
+// firestore examples :
+firestore
+  .collection("users")
+  .doc("7y3uKAABvlJjg5s5Coti")
+  .collection("cardItems")
+  .doc("5hjKFypGSi8Km04WixDg");
+firestore.doc("/users/7y3uKAABvlJjg5s5Coti/cardItems/5hjKFypGSi8Km04WixDg");
+firestore.collection("/users/7y3uKAABvlJjg5s5Coti/cardItems");
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
