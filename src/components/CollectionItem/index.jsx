b406e86ee2ab4371b7537689f94c241ba70d6aea
fadi <fadeomar2015@gmail.com>
2020-11-09 23:26:13 +0200

    connect the component with redux

--- src/components/CollectionItem/index.jsx
+++ src/components/CollectionItem/index.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
 import Button from "../Button";
+import { addItemAction } from "../../redux/casrt/actions";
 
 import "./style.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ id, name, price, imageUrl, addItem, ...rest }) => (
   <div key={id} className="collection-item">
     <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className="collection-footer">
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
-    <Button inverted>Add to cart</Button>
+    <Button
+      inverted
+      onClick={() => addItem({ id, name, price, imageUrl, ...rest })}
+    >
+      Add to cart
+    </Button>
   </div>
 );
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItemAction(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
