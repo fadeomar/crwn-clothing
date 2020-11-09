0b3c9721ded6d17874e99ac9d214eafdadfa1430
fadi <fadeomar2015@gmail.com>
2020-11-09 22:42:19 +0200

    update the CollectionItem component

--- src/components/CollectionItem/index.jsx
+++ src/components/CollectionItem/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../Button";
 
 import "./style.scss";
 
@@ -9,6 +10,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">{price}</span>
     </div>
+    <Button inverted>Add to cart</Button>
   </div>
 );
 
