5d43c5f26f6117c60fa9aab446e73f61ce9c5d9e
fadi <fadeomar2015@gmail.com>
2020-11-11 03:37:44 +0200

    use add item util in the erducer

--- src/redux/casrt/reducer.js
+++ src/redux/casrt/reducer.js
@@ -1,4 +1,5 @@
 import { TOGGLE_CART_HIDDEN, ADD_ITEM } from "./types";
+import addItemUtil from "../../utils/addToCart";
 
 const initialState = {
   hidden: true,
@@ -15,7 +16,7 @@ const cartReducer = (state = initialState, action) => {
     case ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload],
+        cartItems: addItemUtil(state.cartItems, action.payload),
       };
     default:
       return state;
