2068f6c231a430ed7cfa82d181c346cc86d0080c
fadi <fadeomar2015@gmail.com>
2020-11-12 01:38:49 +0200

    create new action for decrease items

--- src/redux/casrt/actions.js
+++ src/redux/casrt/actions.js
@@ -1,5 +1,20 @@
-import { TOGGLE_CART_HIDDEN, ADD_ITEM } from "./types";
+import {
+  TOGGLE_CART_HIDDEN,
+  ADD_ITEM,
+  REMOVE_ITEM,
+  DECREASE_ITEM,
+} from "./types";
 
 export const toggleCartHidden = () => ({ type: TOGGLE_CART_HIDDEN });
 
 export const addItemAction = (item) => ({ type: ADD_ITEM, payload: item });
+
+export const removeItemAction = (item) => ({
+  type: REMOVE_ITEM,
+  payload: item,
+});
+
+export const decreaseItemAction = (item) => ({
+  type: DECREASE_ITEM,
+  payload: item,
+});
